5b4ca2e9d08088bcd91659d9c52ef6b63dd7de49
Poseidon <63670929+poseidon-boi@users.noreply.github.com>
2025-04-25 15:26:56 +0530

    Changed corsoptions to use vercel git url

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -4,7 +4,7 @@ const whitelist = [
 	"http://127.0.0.1:5500",
 	"http://localhost:3500",
 	"http://127.0.0.1:3500",
-	"http://cardsproject-nbig70nmt-armaan-guptas-projects-b425c607.vercel.app/",
+	"http://cardsproject-git-main-armaan-guptas-projects-b425c607.vercel.app/",
 	"https://cardsproject.vercel.app/",
 ];
 const corsOptions = {
